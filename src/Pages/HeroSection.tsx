7b630e64e5c6eabb9fec3fe53a36a2a37330a369
JMakoti <josephmwamuye11@gmail.com>
2025-05-22 13:55:21 +0300

    upd: hero section

--- src/Pages/HeroSection.tsx
+++ src/Pages/HeroSection.tsx
@@ -1,9 +1,28 @@
 import Styles from '../styles/HeroSection.module.css';
+import profile from '../assets/logo.jpg';
+import { Link } from 'react-router-dom';
 
 export default function HeroSection() {
     return (
         <div className={Styles.heroSection}>
-            <h4 className={Styles.heroSectionTitle}>Education</h4>
+            <div className={Styles.heroSectionProfile}>
+                <img src={profile} alt={profile} />
+            </div>
+            <div className={Styles.heroSectionProfileName}>
+                <h1>I'm Joseph Mwamuye</h1>
+                <div className={Styles.heroSectionDescriptions}>
+                    <h2> Software Engineer</h2>
+                    <h2> Web Developer</h2>
+                    <h2> Mobile Developer</h2>
+                </div>
+                <div className={Styles.heroBtns}>
+                    <Link to="/contact" className={Styles.btn}>Contact Me</Link>
+                    <Link to="/contact" className={Styles.btn}>My Projects</Link>
+                    <Link to="/contact" className={Styles.btn}>Download Resume</Link>
+                </div>
+
+            </div>
+
         </div>
     )
 }
