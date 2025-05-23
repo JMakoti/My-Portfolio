1102c57d3bc861f790ec564c4dfda376ff87931d
JMakoti <josephmwamuye11@gmail.com>
2025-05-23 19:49:37 +0300

    RWD: navbar mobile view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,12 +17,12 @@ export default function Navbar() {
                 </div>
 
                 <div className={Styles.nav}>
-                    <Link to="/" className={Styles.link}><h4> <FaHome /> Home</h4></Link>
-                    <Link to="/about"><h4><FaUser /> About</h4></Link>
-                    <Link to="/education"><h4><FaGraduationCap /> Education</h4></Link>
-                    <Link to="/skills"><h4><FaTools /> Skills</h4></Link>
-                    <Link to="/projects"><h4><FaProjectDiagram /> Projects</h4></Link>
-                    <Link to="/contact"><h4><FaEnvelope /> Contact</h4></Link>
+                    <Link to="/" className={Styles.link}><h4> <FaHome /><span>Home</span></h4></Link>
+                    <Link to="/about"><h4><FaUser /> <span> About</span></h4></Link>
+                    <Link to="/education"><h4><FaGraduationCap />  <span>Education</span></h4></Link>
+                    <Link to="/skills"><h4><FaTools />  <span>Skills</span></h4></Link>
+                    <Link to="/projects"><h4><FaProjectDiagram /> <span> Projects</span></h4></Link>
+                    <Link to="/contact"><h4><FaEnvelope />  <span>Contact</span></h4></Link>
                 </div>
             </div>
 
