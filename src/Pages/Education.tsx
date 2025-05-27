663609d26625801c191e374b5c0a101bc788d2d8
JMakoti <josephmwamuye11@gmail.com>
2025-05-27 13:44:49 +0300

    fix: fixed RWD mobile view

--- src/Pages/Education.tsx
+++ src/Pages/Education.tsx
@@ -18,14 +18,14 @@ export default function Education() {
                             <h2>Bachelor of Science in Computer Science</h2>
                             <span>Graduated: 2024</span>
                         </div>
-                        <h3><MdMapsHomeWork />Rongo University</h3>
+                        <h3 className={Styles.sch}><MdMapsHomeWork />Rongo University</h3>
                         <p>
                             Earned a Bachelor's degree in Computer Science, focusing on algorithms, data structures, software engineering, and web development. Gained hands-on experience through various programming projects and boot camps.
                         </p>
                     </div>
                 </div>
-                
             </div>
+
         </div>
 
     )
