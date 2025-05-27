8ee69d3240f00277dd265230c40b7fba08d7d80a
JMakoti <josephmwamuye11@gmail.com>
2025-05-27 15:41:05 +0300

    fix: identify and fixing errors in diff pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,23 @@ import Styles from '../styles/Navbar.module.css';
 import { FaHome, FaUser, FaGraduationCap, FaTools, FaProjectDiagram, FaEnvelope } from 'react-icons/fa';
 import profile from '../assets/logo.jpeg';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+// This custom hook checks if the screen width is less than or equal to 600px (mobile)
+function useIsMobile(breakpoint = 600) {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= breakpoint);
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= breakpoint);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [breakpoint]);
+    return isMobile;
+}
+
 export default function Navbar() {
+
+    const isMobile = useIsMobile();
+
     return (
         <div>
             <div className={Styles.navbar}>
@@ -21,8 +37,12 @@ export default function Navbar() {
                     <Link to="/about"><h4><FaUser /> <span> About</span></h4></Link>
                     <Link to="/education"><h4><FaGraduationCap />  <span>Education</span></h4></Link>
                     <Link to="/skills"><h4><FaTools />  <span>Skills</span></h4></Link>
-                    <Link to="/projects"><h4><FaProjectDiagram /> <span> Projects</span></h4></Link>
-                    <Link to="/contact"><h4><FaEnvelope />  <span>Contact</span></h4></Link>
+                    {!isMobile && (
+                        <>
+                            <Link to="/projects"><h4><FaProjectDiagram /> <span> Projects</span></h4></Link>
+                            <Link to="/contact"><h4><FaEnvelope />  <span>Contact</span></h4></Link>
+                        </>
+                    )}
                 </div>
             </div>
 
