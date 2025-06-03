a5471df0c0662ae5ecf9806fecbdb0d075c36d66
JMakoti <josephmwamuye11@gmail.com>
2025-06-03 15:51:21 +0300

    upd: mapping data.json

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,8 +1,7 @@
 import Styles from '../styles/Portfolio.module.css';
-import portLogo from '../assets/pwani.png';
-import movieLogo from '../assets/movie.png';
 import { FaLink } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import portfolioData from '../data/porfolio.json';
+
 
 export default function Portfolio() {
   return (
@@ -13,49 +12,49 @@ export default function Portfolio() {
       </div>
       <div className={Styles.portSectionCards}>
 
-        <div className={Styles.portSectionCard}>
-          <div className={Styles.cardLogo}>
-            <img src={portLogo} alt="Joseph Mwamuye"></img>
-          </div>
-          <div className={Styles.cardText}>
-            <h4 className={Styles.cardTitle}>Pwani Innovation Week</h4>
-            <span className={Styles.cardSubTitle}>Website</span>
-            <div className={Styles.cardTags}>
-              <span className={Styles.cardTag}>React</span>
-              <span className={Styles.cardTag}>Vercel</span>
-              <span className={Styles.cardTag}>Innovation</span>
+        {portfolioData.projects.map((project, idx) => (
+          <div className={Styles.portSectionCard} key={idx}>
+            <div className={Styles.cardLogo}>
+              <img
+                src={project.image}
+                alt={project.title}
+                className={Styles.portfolioImage}
+              />
             </div>
-            <p className={Styles.cardDesc}>
-              A fully-featured productivity website developed with React, enabling users to see the event, and book the event. It offers intuitive UI.
-            </p>
-            <div className={Styles.cardBtn}>
-              <Link to="https://wall-of-developers-git-group2-munubis-projects.vercel.app/"><FaLink /></Link>
-              {/* <Link to="https://github.com/JMakoti/wall-of-developers"><FaGithub /></Link> */}
+            <div className={Styles.cardText}>
+              <h2 className={Styles.projectTitle}>{project.title}</h2>
+              <span className={Styles.cardSubTitle}>{project.category}</span>
+              <div className={Styles.cardTags}>
+                {project.tags.map((tag, i) => (
+                  <span className={Styles.cardTag} key={i}>
+                    {tag.name}
+                  </span>
+                ))}
+              </div>
+              <p className={Styles.cardDesc}>
+                {project.description}
+              </p>
+              <div className={Styles.cardBtn}>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={Styles.projectBtn}
+                >
+                  <FaLink />
+                </a>
+                {/* <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={Styles.projectBtn}
+                >
+                  GitHub
+                </a> */}
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className={Styles.portSectionCard}>
-          <div className={Styles.cardLogo}>
-            <img src={movieLogo} alt="Portfolio Logo"></img>
-          </div>
-          <div className={Styles.cardText}>
-            <h4 className={Styles.cardTitle}>Movie Theather</h4>
-            <span className={Styles.cardSubTitle}>Website</span>
-            <div className={Styles.cardTags}>
-              <span className={Styles.cardTag}>Javascript</span>
-              <span className={Styles.cardTag}>Vercel</span>
-              <span className={Styles.cardTag}>Movies</span>
-            </div>
-            <p className={Styles.cardDesc}>
-              A fully-featured productivity website developed with Vanilla, enabling users to see the movies possters, and search. It offers interactive UI.
-            </p>
-            <div className={Styles.cardBtn}>
-              <Link to="https://movie-templete-website.vercel.app/"><FaLink /></Link>
-              {/* <Link to="https://github.com/JMakoti/Movie-Templete-Website"><FaGithub /></Link> */}
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
